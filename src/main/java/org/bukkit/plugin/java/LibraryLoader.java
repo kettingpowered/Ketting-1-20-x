f2f5a69920d6d0b52a2219025ec9f1605ed1f7b8
JustRed23 <manuel.bormans@gmail.com>
2023-11-23 15:16:44 +0100

    Yoink the magma implementation of LibraryLoader

--- src/main/java/org/bukkit/plugin/java/LibraryLoader.java
+++ src/main/java/org/bukkit/plugin/java/LibraryLoader.java
@@ -1,128 +1,173 @@
-// CHECKSTYLE:OFF
 package org.bukkit.plugin.java;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.eclipse.aether.DefaultRepositorySystemSession;
-import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.artifact.Artifact;
-import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.aether.collection.CollectRequest;
-import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
-import org.eclipse.aether.graph.Dependency;
-import org.eclipse.aether.impl.DefaultServiceLocator;
-import org.eclipse.aether.repository.LocalRepository;
-import org.eclipse.aether.repository.RemoteRepository;
-import org.eclipse.aether.repository.RepositoryPolicy;
-import org.eclipse.aether.resolution.ArtifactResult;
-import org.eclipse.aether.resolution.DependencyRequest;
-import org.eclipse.aether.resolution.DependencyResolutionException;
-import org.eclipse.aether.resolution.DependencyResult;
-import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
-import org.eclipse.aether.spi.connector.transport.TransporterFactory;
-import org.eclipse.aether.transfer.AbstractTransferListener;
-import org.eclipse.aether.transfer.TransferCancelledException;
-import org.eclipse.aether.transfer.TransferEvent;
-import org.eclipse.aether.transport.http.HttpTransporterFactory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-class LibraryLoader
-{
+class LibraryLoader {
 
     private final Logger logger;
-    private final RepositorySystem repository;
-    private final DefaultRepositorySystemSession session;
-    private final List<RemoteRepository> repositories;
 
-    public LibraryLoader(@NotNull Logger logger)
-    {
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(20);
+
+    public LibraryLoader(@NotNull Logger logger) {
         this.logger = logger;
-
-        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
-        locator.addService( RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class );
-        locator.addService( TransporterFactory.class, HttpTransporterFactory.class );
-
-        this.repository = locator.getService( RepositorySystem.class );
-        this.session = MavenRepositorySystemUtils.newSession();
-
-        session.setChecksumPolicy( RepositoryPolicy.CHECKSUM_POLICY_FAIL );
-        session.setLocalRepositoryManager( repository.newLocalRepositoryManager( session, new LocalRepository( "libraries" ) ) );
-        session.setTransferListener( new AbstractTransferListener()
-        {
-            @Override
-            public void transferStarted(@NotNull TransferEvent event) throws TransferCancelledException
-            {
-                logger.log( Level.INFO, "Downloading {0}", event.getResource().getRepositoryUrl() + event.getResource().getResourceName() );
-            }
-        } );
-        session.setReadOnly();
-
-        this.repositories = repository.newResolutionRepositories( session, Arrays.asList( new RemoteRepository.Builder( "central", "default", "https://repo.maven.apache.org/maven2" ).build() ) );
     }
 
     @Nullable
-    public ClassLoader createLoader(@NotNull PluginDescriptionFile desc)
-    {
-        if ( desc.getLibraries().isEmpty() )
-        {
+    public ClassLoader createLoader(@NotNull PluginDescriptionFile desc) {
+        if (desc.getLibraries().isEmpty()) {
             return null;
         }
-        logger.log( Level.INFO, "[{0}] Loading {1} libraries... please wait", new Object[]
-        {
-            desc.getName(), desc.getLibraries().size()
-        } );
+        logger.log(Level.INFO, "[{0}] Loading {1} libraries... please wait", new Object[]
+                {
+                        desc.getName(), desc.getLibraries().size()
+                });
 
         List<Dependency> dependencies = new ArrayList<>();
-        for ( String library : desc.getLibraries() )
-        {
-            Artifact artifact = new DefaultArtifact( library );
-            Dependency dependency = new Dependency( artifact, null );
+        for (String library : desc.getLibraries()) {
+            String[] args = library.split(":");
+            if (args.length >= 2) {
+                Dependency dependency = new Dependency(args[0], args[1], args[2]);
+                dependencies.add(dependency);
+            }
 
-            dependencies.add( dependency );
         }
 
-        DependencyResult result;
-        try
-        {
-            result = repository.resolveDependencies( session, new DependencyRequest( new CollectRequest( (Dependency) null, dependencies, repositories ), null ) );
-        } catch ( DependencyResolutionException ex )
-        {
-            throw new RuntimeException( "Error resolving libraries", ex );
+        List<File> downloaded = new ArrayList<>();
+
+        for (Dependency dependency : dependencies) {
+            String group = dependency.getGroup().replaceAll("\\.", "/");
+            String fileName = dependency.getName() + "-"
+                    + dependency.getVersion() + ".jar";
+            String mavenUrl = "https://repo1.maven.org/maven2/" + group + "/"
+                    + dependency.getName() + "/"
+                    + dependency.getVersion() + "/" + fileName;
+
+            File file = new File(new File("libraries"), group + "/"
+                    + dependency.getName() + "/"
+                    + dependency.getVersion() + "/" + fileName);
+
+            if (file.exists()) {
+                logger.log(Level.INFO, "[{0}] Found library {1}", new Object[]
+                        {
+                                desc.getName(), file
+                        });
+                downloaded.add(file);
+                continue;
+            }
+
+            Future<Boolean> future = executorService.submit(() -> {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+
+                try {
+                    InputStream inputStream = new URL(mavenUrl).openStream();
+                    writeInputStreamToFile(inputStream, file);
+                    downloaded.add(file);
+                    return true;
+                } catch (IOException e) {
+                    return false;
+                }
+            });
+
+
+            try {
+                boolean success = future.get();
+                if (success) {
+                    logger.log(Level.INFO, "[{0}] Downloading Library {1}", new Object[]
+                            {
+                                    desc.getName(), mavenUrl
+                            });
+                }
+
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         List<URL> jarFiles = new ArrayList<>();
-        for ( ArtifactResult artifact : result.getArtifactResults() )
-        {
-            File file = artifact.getArtifact().getFile();
-
+        for (File file : downloaded) {
             URL url;
-            try
-            {
+            try {
                 url = file.toURI().toURL();
-            } catch ( MalformedURLException ex )
-            {
-                throw new AssertionError( ex );
+            } catch (MalformedURLException ex) {
+                throw new AssertionError(ex);
             }
 
-            jarFiles.add( url );
-            logger.log( Level.INFO, "[{0}] Loaded library {1}", new Object[]
-            {
-                desc.getName(), file
-            } );
+            jarFiles.add(url);
+            logger.log(Level.INFO, "[{0}] Loaded library {1}", new Object[]
+                    {
+                            desc.getName(), file
+                    });
         }
 
-        URLClassLoader loader = new URLClassLoader( jarFiles.toArray( new URL[ jarFiles.size() ] ), getClass().getClassLoader() );
-
-        return loader;
+        return new URLClassLoader(jarFiles.toArray(new URL[0]), getClass().getClassLoader());
     }
+
+
+    private static void writeInputStreamToFile(InputStream inputStream, File file) {
+        try (inputStream) {
+            try (FileOutputStream outputStream = new FileOutputStream(file)) {
+                byte[] buffer = new byte[8 * 1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+
+    public class Dependency {
+
+        private final String group;
+        private final String name;
+        private final String version;
+
+        public Dependency(String group, String name, String version) {
+            this.group = group;
+            this.name = name;
+            this.version = version;
+        }
+
+        public String getGroup() {
+            return group;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        @Override
+        public String toString() {
+            return "Dependency{" +
+                    "group='" + group + '\'' +
+                    ", name='" + name + '\'' +
+                    ", version='" + version + '\'' +
+                    '}';
+        }
+    }
+
 }
