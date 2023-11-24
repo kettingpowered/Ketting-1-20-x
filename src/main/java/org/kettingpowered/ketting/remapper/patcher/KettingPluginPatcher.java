167804a1dcfe4780d06fc720e15bc9cf0e7dba84
JustRed23 <manuel.bormans@gmail.com>
2023-11-24 11:12:57 +0100

    Fix patcher

--- src/main/java/org/kettingpowered/ketting/remapper/patcher/KettingPluginPatcher.java
+++ src/main/java/org/kettingpowered/ketting/remapper/patcher/KettingPluginPatcher.java
@@ -1,5 +1,6 @@
 package org.kettingpowered.ketting.remapper.patcher;
 
+import com.google.common.reflect.ClassPath;
 import io.izzel.arclight.api.PluginPatcher;
 import org.kettingpowered.ketting.core.Ketting;
 import org.kettingpowered.ketting.remapper.ClassLoaderRemapper;
@@ -7,8 +8,6 @@ import org.kettingpowered.ketting.remapper.GlobalClassRepo;
 import org.kettingpowered.ketting.remapper.PluginTransformer;
 import org.objectweb.asm.tree.ClassNode;
 
-import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -32,7 +31,6 @@ public class KettingPluginPatcher implements PluginTransformer {
         List<PluginPatcher> list = new ArrayList<>();
         Ketting.LOGGER.info("Loading patches...");
 
-        //TODO: this does not work yet
         getPatchesFromPackage(list, "org.kettingpowered.ketting.patches");
 
         list.sort(Comparator.comparing(PluginPatcher::priority));
@@ -43,49 +41,19 @@ public class KettingPluginPatcher implements PluginTransformer {
 
     private static void getPatchesFromPackage(List<PluginPatcher> list, String packageName) {
         try {
-            packageName = packageName.replace('.', '/');
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            final URL pkgResource = classLoader.getResource(packageName);
-
-            if (pkgResource == null) {
-                Ketting.LOGGER.warn("Patch package {} does not exist", packageName);
-                return;
-            }
-
-            File packageDir = new File(pkgResource.getFile());
-            if (!packageDir.isDirectory()) {
-                Ketting.LOGGER.warn("Patch package {} is not a directory", packageName);
-                return;
-            }
-
-            final File[] files = packageDir.listFiles();
-            if (files == null || files.length == 0) {
-                Ketting.LOGGER.warn("Patch package {} is empty", packageName);
-                return;
-            }
-
-            for (File file : files) {
-                if (file.isFile() && file.getName().endsWith(".class")) {
-                    // Extract class name and load the class
-                    String className = packageName + "." + file.getName().substring(0, file.getName().lastIndexOf('.'));
-                    Class<?> clazz;
-
-                    try {
-                        clazz = Class.forName(className);
-                    } catch (ClassNotFoundException e) {
-                        Ketting.LOGGER.warn("Failed to load class: " + className);
-                        continue;
-                    }
-
-                    if (!PluginPatcher.class.isAssignableFrom(clazz)) {
-                        Ketting.LOGGER.warn("Skipped non-patcher class: " + className);
-                        continue;
-                    }
-
-                    PluginPatcher patcher = (PluginPatcher) clazz.newInstance();
-                    list.add(patcher);
-                }
-            }
+            ClassPath.from(ClassLoader.getSystemClassLoader())
+                    .getTopLevelClassesRecursive(packageName)
+                    .forEach(info -> {
+                        try {
+                            final Class<?> aClass = Class.forName(info.getName());
+                            if (PluginPatcher.class.isAssignableFrom(aClass)) {
+                                PluginPatcher patcher = (PluginPatcher) aClass.newInstance();
+                                list.add(patcher);
+                            }
+                        } catch (Exception e) {
+                            Ketting.LOGGER.warn("Failed to load patcher {}", info.getName(), e);
+                        }
+                    });
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
