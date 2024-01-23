0bddae0e9afc5b86c14a7fac53260720cd737297
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-01-24 00:24:49 +0100

    [skip-ci] Fix Signs not opening their ui when placed

--- src/main/java/net/minecraftforge/common/ForgeHooks.java
+++ src/main/java/net/minecraftforge/common/ForgeHooks.java
@@ -582,7 +582,7 @@ public class ForgeHooks
             level.captureBlockSnapshots = true;
 
         ItemStack copy = itemstack.copy();
-        InteractionResult ret = itemstack.getItem().useOn(context);
+        InteractionResult ret = itemstack.onItemUse(context, (c)->itemstack.getItem().useOn(context));
         if (itemstack.isEmpty())
             ForgeEventFactory.onPlayerDestroyItem(player, copy, context.getHand());
 
