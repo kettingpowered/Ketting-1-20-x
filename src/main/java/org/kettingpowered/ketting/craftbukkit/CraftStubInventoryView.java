f54a16619a46990d3b5cce345e90a411cd5e6339
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-02-26 15:51:38 +0100

    Make CraftStubInventoryView return a bukkit Player
    Essentials apparently casts the result of getPlayer to a Player.
    
    Addresses part of: #129

--- src/main/java/org/kettingpowered/ketting/craftbukkit/CraftStubInventoryView.java
+++ src/main/java/org/kettingpowered/ketting/craftbukkit/CraftStubInventoryView.java
@@ -1,40 +1,14 @@
 package org.kettingpowered.ketting.craftbukkit;
 
-import com.mojang.authlib.GameProfile;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.dedicated.DedicatedServer;
-import net.minecraft.world.entity.player.Player;
-import org.bukkit.craftbukkit.v1_20_R1.entity.CraftHumanEntity;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.UUID;
-
 public class CraftStubInventoryView extends InventoryView {
     public static final CraftStubInventoryView INSTANCE = new CraftStubInventoryView();
     private CraftStubInventoryView(){}
-    private static final HumanEntity StubEntity = 
-            new CraftHumanEntity(
-                    DedicatedServer.getServer().server, 
-                    new Player(
-                            DedicatedServer.getServer().overworld(), 
-                            BlockPos.ZERO,
-                            0.0F,
-                            new GameProfile(new UUID(0, 0), "test-mock-player")
-                    ) {
-                        @Override
-                        public boolean isSpectator() {
-                            return false;
-                        }
-
-                        @Override
-                        public boolean isCreative() {
-                            return false;
-                        }
-                    });
     @Override
     public @NotNull Inventory getTopInventory() {
         return CraftStubInventory.INSTANCE;
@@ -47,7 +21,7 @@ public class CraftStubInventoryView extends InventoryView {
 
     @Override
     public @NotNull HumanEntity getPlayer() {
-        return StubEntity;
+        return CraftStubPlayer.INSTANCE;
     }
 
     @Override
