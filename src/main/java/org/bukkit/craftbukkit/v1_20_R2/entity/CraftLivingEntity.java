816e66e6cf9541bdb764e7c0408c42e61d23f8f3
JustRed23 <manuel.bormans@gmail.com>
2024-03-11 15:41:26 +0100

    Effect mixin patches [1/2]

--- src/main/java/org/bukkit/craftbukkit/v1_20_R2/entity/CraftLivingEntity.java
+++ src/main/java/org/bukkit/craftbukkit/v1_20_R2/entity/CraftLivingEntity.java
@@ -360,7 +360,8 @@ public class CraftLivingEntity extends CraftEntity implements LivingEntity {
 
     @Override
     public boolean addPotionEffect(PotionEffect effect, boolean force) {
-        getHandle().addEffect(new MobEffectInstance(CraftPotionEffectType.bukkitToMinecraft(effect.getType()), effect.getDuration(), effect.getAmplifier(), effect.isAmbient(), effect.hasParticles()), EntityPotionEffectEvent.Cause.PLUGIN);
+        getHandle().prepareAddEffect(EntityPotionEffectEvent.Cause.PLUGIN); //Ketting
+        getHandle().addEffect(new MobEffectInstance(CraftPotionEffectType.bukkitToMinecraft(effect.getType()), effect.getDuration(), effect.getAmplifier(), effect.isAmbient(), effect.hasParticles()));
         return true;
     }
 
