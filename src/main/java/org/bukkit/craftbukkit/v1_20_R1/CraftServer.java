1e65d611a007c8984fa581128b2b23914b190101
JustRed23 <manuel.bormans@gmail.com>
2024-03-12 17:31:33 +0100

    Use better world init handling

--- src/main/java/org/bukkit/craftbukkit/v1_20_R1/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/v1_20_R1/CraftServer.java
@@ -1183,7 +1183,7 @@ public final class CraftServer implements Server {
             worldKey = ResourceKey.create(Registries.DIMENSION, new ResourceLocation(name.toLowerCase(java.util.Locale.ENGLISH)));
         }
 
-        ServerLevel internal = ServerLevel.init(console, console.executor, worldSession, worlddata, worldKey, worlddimension, getServer().progressListenerFactory.create(11),
+        ServerLevel internal = new ServerLevel(console, console.executor, worldSession, worlddata, worldKey, worlddimension, getServer().progressListenerFactory.create(11),
                 worlddata.isDebugWorld(), j, creator.environment() == Environment.NORMAL ? list : ImmutableList.of(), true, console.overworld().getRandomSequences(), creator.environment(), generator, biomeProvider);
 
         if (!(worlds.containsKey(name.toLowerCase(java.util.Locale.ENGLISH)))) {
