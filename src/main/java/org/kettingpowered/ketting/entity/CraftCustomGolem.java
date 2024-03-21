721934a4f5111de22049328016dc37e28431f84a
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-03-21 13:38:07 +0100

    Fixup Patches #3

--- src/main/java/org/kettingpowered/ketting/entity/CraftCustomGolem.java
+++ src/main/java/org/kettingpowered/ketting/entity/CraftCustomGolem.java
@@ -1,8 +1,8 @@
 package org.kettingpowered.ketting.entity;
 
 import net.minecraft.world.entity.animal.AbstractGolem;
-import org.bukkit.craftbukkit.v1_20_R1.CraftServer;
-import org.bukkit.craftbukkit.v1_20_R1.entity.CraftGolem;
+import org.bukkit.craftbukkit.v1_20_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_20_R2.entity.CraftGolem;
 
 public class CraftCustomGolem extends CraftGolem {
 
