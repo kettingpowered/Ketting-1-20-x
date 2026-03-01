ba53d818e015c7a3962c212a183c513734dffd30
JustRed23 <manuel.bormans@gmail.com>
2026-03-01 20:43:12 +0100

    Don't print wrapping error on moonlight lib's FakeServerLevel

--- src/main/java/org/kettingpowered/ketting/utils/DelegateWorldInfo.java
+++ src/main/java/org/kettingpowered/ketting/utils/DelegateWorldInfo.java
@@ -251,7 +251,8 @@ public class DelegateWorldInfo extends PrimaryLevelData {
         } else if (levelData instanceof net.minecraft.world.level.storage.DerivedLevelData derived) {
             serverLevelData = org.kettingpowered.ketting.utils.DelegateWorldInfo.wrap(derived);
         } else if (levelData instanceof ServerLevelData serverlevelData1) {
-            org.kettingpowered.ketting.core.Ketting.LOGGER.warn("Could not wrap level data, this can cause some problems with bukkit", new UnsupportedOperationException());
+            if (!levelData.getClass().getName().startsWith("net.mehvahdjukaar.moonlight.api")) //Moonlight Lib is fine
+                org.kettingpowered.ketting.core.Ketting.LOGGER.warn("Could not wrap level data, this can cause some problems with bukkit", new UnsupportedOperationException());
             serverLevelData = serverlevelData1;
         } else {
             Ketting.LOGGER.error("Could not get a ServerLevelData from a WritableLevelData in Level constructor", new IllegalArgumentException("A WritableLevelData in the Level constructor wasn't a ServerLevelData"));
