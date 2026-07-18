fc9d065fb82f16b13f20d84a5387ad15d25f7b9f
JustRed23 <manuel.bormans@gmail.com>
2026-07-18 15:41:19 +0200

    Add config option for splitting world names between forge and bukkit

--- src/main/java/org/kettingpowered/ketting/config/KettingConfig.java
+++ src/main/java/org/kettingpowered/ketting/config/KettingConfig.java
@@ -23,7 +23,7 @@ public class KettingConfig extends AbstractConfig {
                 "Github: https://github.com/kettingpowered/",
                 "Discord: https://discord.kettingpowered.org/",
                 ""
-        }, fileName, 4);
+        }, fileName, 5);
         load();
     }
 
@@ -37,7 +37,8 @@ public class KettingConfig extends AbstractConfig {
     public final BooleanValue SILENCE_PARSING_ERRORS = new BooleanValue("forge.silence_parsing_errors", true, "If true, this will minimize some general parsing errors from appearing in the console");
 
     public final BooleanValue MERGE_WORLD_SYSTEMS = new BooleanValue("ketting.merge_world_systems", false, "If true, this will attempt to merge both the Forge and Bukkit world system into one, making dimensions exist in the world folder, and Bukkit worlds in their own folder.");
-    
+    public final BooleanValue SPLIT_WORLD_NAMES = new BooleanValue("ketting.split_world_names", false, "If true, this would change dimension names ONLY on the Bukkit side, making Bukkit believe that world/DIM-1 actually is world_nether");
+
     public final BooleanValue HALT_EXIT = new BooleanValue("ketting.halt.exit", false, "If true, Ketting will call System.exit(255), once it is supposed to regularly shutdown. This is useful, if some plugins or mods keep the server up unintentionally.");
     public final BooleanValue HALT_HALT = new BooleanValue("ketting.halt.halt", true, "If true, Ketting will call Runtime.getRuntime().halt(), once it is supposed to regularly shutdown. Overrides 'ketting.halt.exit'. THIS SKIPS SHUTDOWN HOOKS. SOME STUFF MIGHT BREAK! IF YOU CAN, ENABLE 'ketting.halt.exit' INSTEAD!");
     public final IntValue HALT_THREADDUMP_SLEEP = new IntValue("ketting.halt.thread_dump.sleep_time", 5000, "If true, Ketting will sleep this many seconds before printing the thread-dump.");
